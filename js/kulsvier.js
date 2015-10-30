b93e0f7c1e13b4915344caa6dabf28957a6b3a95
Taus Soe <taus.soe@gmail.com>
2015-10-30 11:16:51 +0100

    rettelser

--- js/kulsvier.js
+++ js/kulsvier.js
@@ -1,5 +1,10 @@
 $(function(){
     $('#menuBtn').click(function(){
         $('.menu-container').toggleClass('active');
+        if ($('.menu-container').hasClass('active')){
+            $('#menuBtn').text('Close')
+        } else {
+            $('#menuBtn').text('Menu')
+        }
     });
 })
